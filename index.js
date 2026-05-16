d0361cc0b9ba8ffc951abd1b98126f8b753e3cd2
17099-netizen <17099@dt.ac.th>
2026-05-17 01:28:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const puppeteer = require('puppeteer');
 
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(
+    'https://dkt.gt.tc/admin/cron_uniform?i=1',
+    {
+      waitUntil: 'networkidle2',
+      timeout: 0
+    }
+  );
+
+  console.log('SUCCESS');
+
+  await browser.close();
+})();
